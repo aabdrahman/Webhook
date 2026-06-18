319f3d7c4b018fb85c67e5dee872942524ccb4e2
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-06-18 22:38:39 +0100

    fix: Add Http Status Code to generic response

--- WebHook.Core/DataTransferObjects/GenericResponse.cs
+++ WebHook.Core/DataTransferObjects/GenericResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Net;
+using System.Text.Json.Serialization;
 
 namespace WebHook.Core.DataTransferObjects;
 
@@ -12,18 +13,21 @@ public class GenericResponse<T> where T : class
     private bool IsSuccessful { get; set; }
     [JsonPropertyName("errorDetail")]
     private ErrorDetail? ErrorDetail { get; set; } = null;
+    [JsonPropertyName("httpStatusCode")]
+    private HttpStatusCode HttpStatusCode { get; set; }
 
-    public GenericResponse(T? responseData, string responseMessage, bool isSuccessful, ErrorDetail? errorDetail = null)
+    public GenericResponse(T? responseData, string responseMessage, HttpStatusCode httpStatusCode, bool isSuccessful, ErrorDetail? errorDetail = null)
     {
         ResponseData = responseData;
         ResponseMessage = responseMessage;
         IsSuccessful = isSuccessful;
         ErrorDetail = errorDetail;
+        HttpStatusCode = httpStatusCode;
     }
 
-    public static GenericResponse<T> Success(T? responseData, string responseMessage) => new GenericResponse<T>(responseData, responseMessage, true);
+    public static GenericResponse<T> Success(T? responseData, string responseMessage, HttpStatusCode httpStatusCode) => new GenericResponse<T>(responseData, responseMessage, httpStatusCode, true);
 
-    public static GenericResponse<T> Failure(T? responseData, string responseMessage, ErrorDetail? errorDetail = null) => new GenericResponse<T>(responseData, responseMessage, false, errorDetail);
+    public static GenericResponse<T> Failure(T? responseData, string responseMessage, HttpStatusCode httpStatusCode, ErrorDetail? errorDetail = null) => new GenericResponse<T>(responseData, responseMessage, httpStatusCode, false, errorDetail);
 }
 
 
