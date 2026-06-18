b1b793182d3ff502191a010571bd2b8a8d45cb40
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-06-18 20:52:28 +0100

    feat: Add Swagger and logger configuration

--- WebHook.Api/Program.cs
+++ WebHook.Api/Program.cs
@@ -1,10 +1,26 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Serilog Logger implementation
+string logFromConfig = builder.Configuration.GetValue<string>("");
+string logFilePath = string.IsNullOrWhiteSpace(logFromConfig) ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log-.txt") : Path.Combine(logFromConfig, "log-.txt");
+
+Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()   
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(path: logFilePath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true,
+                                    fileSizeLimitBytes: 5_000_000, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose, 
+                                    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.ffff zzzz} || {Level:u3}] || [{ClassName}].[{MethodName}] - {Message:lj}{NewLine}{Exception}{NewLine}")
+                    .CreateLogger();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -13,6 +29,16 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseReDoc(opts =>
+{
+    opts.SpecUrl("/openapi/v1.json");
+});
+
+app.UseSwaggerUI(opts =>
+{
+    opts.SwaggerEndpoint("/openapi/v1.json", "Webhook API V1");
+});
 
 app.UseHttpsRedirection();
 
---
+++ WebHook.Api/ServiceExtensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace WebHook.Api.ServiceExtensions;
+
+public static class ApplicationServiceExtensions
+{
+    internal static void ConfigureSwagger(this IServiceCollection services)
+    {
+        //services.AddSwaggerGen(opts =>
+        //{
+        //    opts.SwaggerDoc("v1", new Microsoft.OpenApi()
+        //    {
+        //        Contact = new Microsoft.OpenApi.OpenApiContact() { Email = "akandeabdrahman@gmail.com", Name = "Akande Abdrahman", Url = new Uri("https://github.com/aabdrahman") },
+        //        Description = "API for webhook implementation",
+        //        Title = "Webhook API",
+        //        Version = "v1",
+        //        Summary = "Swager documentation for the webhook api"
+        //    });
+        //});
+
+    }
+}
