3cbaf27b4a85d3010bfe182b4f4f8518505ba7d1
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-06-21 20:25:00 +0100

    feat: Add Security implementatiosn(encryption and secret key generator)

---
+++ WebHook.Infrastructure/Security/SecretKeyGeneratorService.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using WebHook.Core.Interfaces.Helpers;
+
+namespace WebHook.Infrastructure.Security;
+
+public sealed class SecretKeyGeneratorService : ISecretKeyGenerator
+{
+    public string GenerateKey(int secretKeySize = 32)
+    {
+        var rndNumBytes = new byte[secretKeySize];
+
+        using (var randNum = RandomNumberGenerator.Create())
+        {
+            randNum.GetBytes(rndNumBytes);
+        }
+
+        return Convert.ToHexString(rndNumBytes);
+    }
+}
