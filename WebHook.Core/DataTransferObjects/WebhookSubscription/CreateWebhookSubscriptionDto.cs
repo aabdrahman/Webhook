de48508a54e017188c1a4db9b550c33e8070cf20
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-06-28 11:10:20 +0100

    feat: Create Webhook Subscription Mapper and Dto

---
+++ WebHook.Core/DataTransferObjects/WebhookSubscription/CreateWebhookSubscriptionDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebHook.Core.DataTransferObjects.WebhookSubscription;
+
+public record class CreateWebhookSubscriptionDto
+{
+    [Required(ErrorMessage = "Subscriber Name is a required field.")]
+    [Display(Name = "Subscriber Name")]
+    public string SubscriberName { get; set; }
+    [Required(ErrorMessage = "Kindly provide one or more events to subscribe to.")]
+    [Range(1, int.MaxValue, ErrorMessage = "One or more events are to be subscribed to.")]
+    public List<string> SubscribedEvents { get; set; }
+    [Required(ErrorMessage = "Kindly provide one or more fields to subscribe to.")]
+    [Range(1, int.MaxValue, ErrorMessage = "One or more events are to be subscribed to.")]
+    public List<string> SubscribedFields { get; set; }
+    [Required(ErrorMessage = "Cal Back Url is a required field.")]
+    [Display(Name = "Call Back Url")]
+    public string CallBackUrl { get; set; }
+}
