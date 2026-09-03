39b2c769e61aedba4ee11344f52492a99235ada6
Abdrahman Akande <akandeabdrahman@gmail.com>
2026-09-03 22:16:18 +0100

    feat: Add Migration for seeding default roles

--- WebHook.Core/Entities/Role.cs
+++ WebHook.Core/Entities/Role.cs
@@ -6,7 +6,7 @@ namespace WebHook.Core.Entities;
 
 public class Role : IdentityRole<Guid>
 {
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? CreatedAt { get; set; }
     public string Description { get; set; }
     public bool IsActive { get; set; } = true;
 }
